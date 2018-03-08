38da2152d5a566b3454732bfeb5e1239360c7c3d
Lionel Rowe <lionel.rowe@gmail.com>
2018-03-08 21:59:19 +0800

    step 3

--- myApp.js
+++ myApp.js
@@ -16,12 +16,15 @@ console.log('Hello World');
 
 // app.listen(3000);
 
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`);
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello Express');
+// });
 
 /** 3) Serve an HTML file */
 
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
 
 /** 4) Serve static assets  */
 
